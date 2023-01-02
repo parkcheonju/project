b3f3825c2e797b3d0952d58f03e52dc01a06a64a
parkcheonju <cjswne@gmail.com>
2023-01-02 18:14:15 +0900

    ver.13 commit

--- js/tab.js
+++ js/tab.js
@@ -1,15 +1,15 @@
-let taps=$(".ct__bt>a");
-let panels=$(".ct__bottom>div");
+let menus = $(".ct__bt>a");
+let panels = $(".ct__bottom>div");
 
-taps.eq(0).addClass("active");
+menus.eq(0).addClass("active");
 panels.eq(0).show();
 
-taps.click(function(e){
-    e.preventDefault();
-    let tg=$(this);
-    let currentLink=tg.attr("href");
-
-    taps.removeClass("active");
-    $(currentLink).show();
-    panels.hide();
+menus.click(function (e) {
+  e.preventDefault();
+  let tg = $(this);
+  let currentLink = tg.attr("href");
+  menus.removeClass("active");
+  tg.addClass("active");
+  panels.hide();
+  $(currentLink).show();
 });
