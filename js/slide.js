708de89b4f2c0b319b13300ad170ad6b8063ac2a
parkcheonju <cjswne@gmail.com>
2023-02-21 18:23:39 +0900

    slide change commit

--- js/slide.js
+++ js/slide.js
@@ -1,47 +1,14 @@
-$(function () { 
-    const visual = $("#banner_slide>ul>li>div"); 
-    const button = $("#slide_bt>li"); 
-    let current = 0; 
-    let btnIdx = 0; 
-    let id; 
-    const speed = 3000;
-    button.click(function () {
-		btnIdx= $(this).index();
-		button.removeClass("on");
-		$(this).addClass("on");
-		move();
-});
-
-//시간마다실행
-timer();
-function timer() {
-    id = setInterval(function (){ 
-        let next = current + 1; 
-        if (next == visual.length) {
-            next = 0;
-        }
-        button.eq(next).trigger("click");
-    }, speed);
-};
-
-//이동시키는 함수
-function move() {
-    if (current == btnIdx) return;
-    let cu = visual.eq(current);
-    let ne = visual.eq(btnIdx);
-    cu.css("left", "0").stop().animate({ left: "-100%" });
-    ne.css("left", "100%").stop().animate({ left: "0%" });
-    current = btnIdx;
-}
-
-clearAuto();
-function clearAuto() { 
-    $("#banner_slide,#slide_bt").mouseenter(function (){
-        clearInterval(id);
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+      mode:'horizontal',
+      speed:500,
+      captions:true,
+      controls:true,
+      auto:true,
+      nextText:'<i class="fa-solid fa-arrow-right"></i>',
+      prevText:'<i class="fa-solid fa-arrow-left"></i>',
+      autoControls:true,
+      startText:'◼',
+      stopText:'▶',
     });
-    $("#banner_slide,#slide_bt").mouseleave(function (){
-        timer();
-    }); 
-};
-
 });
